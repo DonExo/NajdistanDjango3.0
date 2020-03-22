c492979f8865091b8d61304b12a795eda2cbd6bc
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-03-22 20:25:03 +0100

    added debug toolbar

--- najdistandjango30/urls.py
+++ najdistandjango30/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include, re_path
 from users import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
 
     path('admin/', admin.site.urls),
 ]

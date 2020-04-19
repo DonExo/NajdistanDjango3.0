e82e0b6aba8da9d37570dad4692045ebee6ec727
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-04-19 02:08:04 +0200

    Deleted comments

--- listings/forms.py
+++ listings/forms.py
@@ -19,26 +19,3 @@ class ListingCreateForm(forms.ModelForm):
 
 class ListingUpdateForm(ListingCreateForm):
     title = forms.CharField(disabled=True)
-
-
-# class ImageUploadForm(forms.Form):
-#     images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-
-    # def clean(self):
-    #     print("clean()")
-    #     super().clean()
-    #     images = self.cleaned_data.get('images')
-    #     print(images)
-    #     # return
-    #     return images
-
-    # def clean_images(self):
-    #     print("clean_images")
-    #     files = self.files.getlist('images')
-    #     import imghdr
-    #     for file in files:
-    #         print(imghdr.what(file.name))
-    #         # print(file.name, type(file), dir(file))
-    #
-    #     # print(files)
-    #     return files

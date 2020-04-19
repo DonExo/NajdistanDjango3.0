c258e3cfa42a3e6658c56c9a0859b9ecdf8f4ca3
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-04-19 02:06:53 +0200

    Removed commented out route

--- listings/urls.py
+++ listings/urls.py
@@ -5,7 +5,6 @@ from listings import views
 
 
 urlpatterns = [
-    # path('', views.ListingListView.as_view(), name='list'),
     path('create/', views.ListingCreateView.as_view(), name='create'),
     path('<slug:slug>/', views.ListingDetailView.as_view(), name='detail'), # Remove the last slash if Django complains
     path('<slug:slug>/update/', views.ListingUpdateView.as_view(), name='update'),

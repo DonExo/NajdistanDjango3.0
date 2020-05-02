db2e64e1e40245ce8f0960b10ee7111591ff0d86
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-05-02 19:20:55 +0200

    Added dummy placeholder on the 'title'

--- listings/filters.py
+++ listings/filters.py
@@ -1,10 +1,13 @@
 import django_filters as filters
+
+from django.forms.widgets import TextInput
+
 from django_filters.widgets import RangeWidget
 
 from .models import Listing
 
 class ListingFilter(filters.FilterSet):
-    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'zelka'}))
     price = filters.RangeFilter(field_name='price', label='Price range', widget=RangeWidget)
 
     #@TODO: Make sure to add all the needed filters with right properties (i.e. quadrature, balcony etc)

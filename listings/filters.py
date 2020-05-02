05d62ae11b76f17281f1332980e38e9396d04983
Igor Veleski <vele.igor@gmail.com>
2020-05-02 21:59:58 +0200

    fe - add more images to listing in sidebar

--- listings/filters.py
+++ listings/filters.py
@@ -7,8 +7,8 @@ from django_filters.widgets import RangeWidget
 from .models import Listing
 
 class ListingFilter(filters.FilterSet):
-    title = filters.CharFilter(field_name='title', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'zelka'}))
-    price = filters.RangeFilter(field_name='price', label='Price range', widget=RangeWidget)
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Enter address e.g. street, city and state or zip'}))
+    price = filters.RangeFilter(field_name='price', label='Price range', widget=RangeWidget(attrs={'placeholder': 'Price range'}))
 
     #@TODO: Make sure to add all the needed filters with right properties (i.e. quadrature, balcony etc)
 

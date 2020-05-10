f3b53509d36e5bbb365ef284ea7efb08c64ca395
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-05-10 22:25:58 +0200

    Removed unused (yet) ModelAdmin from being displayed

--- reports/admin.py
+++ reports/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from .models import CommentReport, ListingReport
 
 
-@admin.register(CommentReport)
+# @admin.register(CommentReport)
 class CommentReportAdmin(admin.ModelAdmin):
     list_display = ('comment', 'reason', 'reporter')
     search_fields = ('reporter', )
 
 
-@admin.register(ListingReport)
+# @admin.register(ListingReport)
 class ListingReportAdmin(admin.ModelAdmin):
     list_display = ('listing', 'reporter', 'reason')
     search_fields = ('listing', 'reporter')

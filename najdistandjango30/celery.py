98c3007cfd4491a6bbe75aca65c24fbf7248ad88
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-05-10 22:44:40 +0200

    comment

--- najdistandjango30/celery.py
+++ najdistandjango30/celery.py
@@ -20,8 +20,9 @@ app.autodiscover_tasks()
 ##################
 # Make sure you have rabbitmq (or other broker) up and running
 # sudo rabbitmq-server
+# redis-server && redis-cli
 
 # Make sure to have the celery worker up and running
 # celery -A najdistandjango30 worker --loglevel=info
-# or
+# or (NOT A PRODUCTION-READY VARIANT !!!)
 # celery -A najdistandjango30 worker --beat --scheduler django --loglevel=info

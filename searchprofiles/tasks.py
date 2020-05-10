4fcfeb1f6b5e4fffc079605b87c0563e99c06344
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-05-10 22:52:18 +0200

    reworded the main celery tasks.py file a bit

--- searchprofiles/tasks.py
+++ searchprofiles/tasks.py
@@ -24,23 +24,28 @@ def get_time_offset(value):
 
 @shared_task
 def send_info_to_user(sp_pk, listings_pk):
+    # Make this a real INFO e-mail for the user
     sp = SearchProfiles.objects.get(pk=sp_pk)
     sp.user.email_user("SP Subject", f"A total of {len(listings_pk)} have been found for your SP {sp.title}")
     print(f"Active: [[{sp.title}]] - Sending info to {sp.user.email} with {len(listings_pk)} new listings")
 
 @shared_task
-def get_new_listings(value): # i.e. 'daily', 'weekly'
+def get_new_listings(value):
+    # Add some debugging eventually?
     print(f"Receiving value '{value}'")
-    sps = SearchProfiles.objects.filter(frequency=value, is_active=True)
-    if not sps.exists():
-        print(f"No active users with '{value}' SP. EXITING")
+    search_profiles = SearchProfiles.objects.filter(frequency=value, is_active=True)
+    if not search_profiles.exists():
+        print(f"No active '{value}' Search Profiles with. EXITING")
         return
 
     offset = get_time_offset(value)
 
     # TODO: This should rather be "approved_at__gte", but that field does not exists in the model yet
-    listings_pk = Listing.objects.filter(created_at__gte=offset).values_list('pk', flat=True)
-    print(f"Found {len(listings_pk)} listings!")
+    # TODO: Make this a real Search profile with all relevant data
+    # TODO: Exclude their own Listings (if any)
+    matching_listings_pks = Listing.objects.filter(created_at__gte=offset).values_list('pk', flat=True)
+    print(f"Found {len(matching_listings_pks)} listings!")
 
-    for sp in sps:
-        send_info_to_user.delay(sp.pk, list(listings_pk))
+    for search_profile in search_profiles:
+        send_info_to_user.delay(search_profile.pk, list(matching_listings_pks))
+        # JSON-only serializable data, no model objects or QuerySets

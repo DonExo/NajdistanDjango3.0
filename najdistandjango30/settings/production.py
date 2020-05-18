1f9e2746c16b2535061e2a17e227867d3f325f34
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-05-18 12:26:27 +0200

    Change smtp settings for production.py

--- najdistandjango30/settings/production.py
+++ najdistandjango30/settings/production.py
@@ -31,11 +31,11 @@ DATABASES = {
 
 # E-mail settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_USE_TLS = True
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 
 GENERATE_DUMMY_LISTING = False

b0a12b4ba64b55579e360121a9068a66ccde4d2c
DonExo <donfrozex@gmail.com>
2020-05-31 01:17:37 +0200

    Workaround to enable showing 'Inactive' user on Login View. This is Django bug #28645

--- authy/forms.py
+++ authy/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import PasswordResetForm, AuthenticationForm, UsernameField, PasswordChangeForm
 
 from registration.forms import RegistrationFormTermsOfService
@@ -11,6 +12,26 @@ class CustomLoginForm(AuthenticationForm):
     remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput())
     captcha = CustomCaptchaV2Invisible()
 
+    # Workaround for the "Inactive user error" to be shown on Login
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                try:
+                    user_temp = User.objects.get(email=username)
+                except:
+                    user_temp = None
+                if user_temp is not None:
+                    self.confirm_login_allowed(user_temp)
+                else:
+                    raise forms.ValidationError(
+                        self.error_messages['invalid_login'],
+                        code='invalid_login',
+                        params={'username': self.username_field.verbose_name},
+                    )
+        return self.cleaned_data
 
 class CustomRegisterForm(RegistrationFormTermsOfService):
     first_name = forms.CharField(max_length=100)

34ea6d04b4d261a4a57a39b6222a329f83d6545a
DonExo <donfrozex@gmail.com>
2020-05-31 02:36:03 +0200

    typo

--- authy/forms.py
+++ authy/forms.py
@@ -18,7 +18,7 @@ class CustomLoginForm(AuthenticationForm):
     def clean(self):
         self.error_messages.update(
             {'inactive_deleted': 'You have deactivated your account. '
-                                 'Click the "Forgot your password" link to activate it again,'}
+                                 'Click the "Forgot your password" link to activate it again.'}
         )
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')

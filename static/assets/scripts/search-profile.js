26ee7e808cbdd98573e5d4c016e0d829eba75af5
DonExo <donfrozex@gmail.com>
2020-06-06 03:01:36 +0200

    Added Ajax call to change SP status from Account

--- static/assets/scripts/search-profile.js
+++ static/assets/scripts/search-profile.js
@@ -1,21 +1,19 @@
 $(".search-profile .input-checkbox-switch label").on("click", function () {
-    const switchId = $(this).prev().attr("id");
-    const switchPk = $(this).prev().attr("name");
+    const sp_pk = $(this).prev().attr("name");
 
     $.ajax({
         type:"POST",
-        url: `sp/toggle-status`,
+        url: `/sp/toggle/`,
         async: true,
         dataType: "json",
-        data: {"spPk": switchPk}, 
+        data: {"sp_pk": sp_pk},
         success: function(response){
-
+            console.log(response)
         },
         error: function(xhr, status, err) {
           console.log(err);
         },
         complete: function(){
-          console.log('finished');
         }
       });
 });

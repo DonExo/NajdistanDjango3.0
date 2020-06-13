993a3225602cd8839246944d38c2e29d6cbf1a41
Igor Veleski <vele.igor@gmail.com>
2020-06-13 21:46:32 +0200

    fe - add value to search type input

--- static/assets/scripts/custom.js
+++ static/assets/scripts/custom.js
@@ -388,28 +388,31 @@ $(document).ready(function(){
 	/*----------------------------------------------------*/
 	/*  Search Type Buttons
 	/*----------------------------------------------------*/
+	// Positioning indicator arrow
+	function indicatorArrowPosition () {
+		var buttonWidth = $('.search-type label.active').width();
+		var arrowDist = $('.search-type label.active').position().left;
+
+		$('.search-type-arrow').css({
+			'left': arrowDist + (buttonWidth/2),
+			'transition':'left 0.4s cubic-bezier(.87,-.41,.19,1.44)'
+		});
+	}
+
 	function searchTypeButtons() {
 
 		// Radio attr reset
 		$('.search-type label.active input[type="radio"]').prop('checked',true);
 
 		// Positioning indicator arrow
-		var buttonWidth = $('.search-type label.active').width();
-		var arrowDist = $('.search-type label.active').position().left;
-		$('.search-type-arrow').css('left', arrowDist + (buttonWidth/2) );
+		indicatorArrowPosition();
 
 		$('.search-type label').on('change', function() {
 		    $('.search-type input[type="radio"]').parent('label').removeClass('active');
-		    $('.search-type input[type="radio"]:checked').parent('label').addClass('active');
+			$('.search-type input[type="radio"]:checked').parent('label').addClass('active');
+			$('#id_type').attr('value', $(this).data('search-type'));
 
-			// Positioning indicator arrow
-			var buttonWidth = $('.search-type label.active').width();
-			var arrowDist = $('.search-type label.active').position().left;
-
-			$('.search-type-arrow').css({
-				'left': arrowDist + (buttonWidth/2),
-				'transition':'left 0.4s cubic-bezier(.87,-.41,.19,1.44)'
-			});
+			indicatorArrowPosition();
 		});
 
 	}
